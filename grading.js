c09d7e0bc9567a7cc4e7b3b2df4624b8b07aac1b
RadiatingPI46 <152961374+RadiatingPI46@users.noreply.github.com>
2024-09-19 10:37:00 +0300

    Add files via upload

---
+++ grading.js
@@ -0,0 +1,31 @@
+// Grading System
+let score =120
+if(score>=0 && score<=40)
+{
+    console.log("Grade = E");
+    
+}
+else if(score>=41 && score<=49)
+{
+    console.log("Grade = D");
+}
+else if(score>=50 && score<=59)
+{
+    console.log("Grade = C");
+    
+}
+else if(score>=60 && score<=69)
+{
+    console.log("Grade = B");
+    
+}
+else if(score>=70 && score<=100)
+{
+    console.log("Grade = A");
+    
+}
+else
+{
+    console.log("Invalid input");
+    
+}
